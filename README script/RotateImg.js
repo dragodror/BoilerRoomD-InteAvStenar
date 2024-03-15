09a7435d651447e180f599f55902245f0ffb43c4
dragodror <156414475+dragodror@users.noreply.github.com>
2024-03-15 16:42:08 +0100

    Add Rotatefunc

--- README script/RotateImg.js
+++ README script/RotateImg.js
@@ -1,1 +1,11 @@
+//Load input image
+var inputImg = require("luminance")(require("inputImg-image"))
 
+//Allocate storage for result
+var result = require("zeros")([512, 512])
+
+//Rotate the image
+require("image-rotate")(result, baboon, Math.PI / 6.0)
+
+//Save the result
+require("save-pixels")(result, "png").pipe(process.stdout)
